00cf8c69ffaafb3f5906b5ab41d038a94128ac91
Rob Kutschke <kutschke@fnal.gov>
2014-01-13 09:16:53 -0600

    First release of PDT as its own Service.

---
+++ toyExperiment/PDT/ParticleInfo.cc
@@ -0,0 +1,23 @@
+//
+// A minimal particle information class for use with the minimal
+// particle data table class.
+//
+
+#include "toyExperiment/PDT/ParticleInfo.h"
+
+tex::ParticleInfo::ParticleInfo():
+  _id(PDGCode::invalid),
+  _mass(0),
+  _q(0),
+  _name("invalid"){
+}
+
+tex::ParticleInfo::ParticleInfo( PDGCode::type id,
+                                   double        mass,
+				   double        q,
+                                   std::string   name):
+  _id(id),
+  _mass(mass),
+  _q(q),
+  _name(name){
+}
